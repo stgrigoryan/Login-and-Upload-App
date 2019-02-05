ef5c01d3a7815a74cf0248c58ce93207f7d1f20c
stgrigoryan <stepan.grigoryan10@gmail.com>
2019-02-06 01:53:03 +0400

    Added multer, Gridfs to handle image upload, added upload route, added upload form in users' profile

--- server/app.js
+++ server/app.js
@@ -34,6 +34,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+mongoose.set('useNewUrlParser', true);
+
 //Create MongoDB connection
 mongoose
   .connect(
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,18 +2,48 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 const path = require('path');
-const User = require('../models/users');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
+const multer = require('multer');
+const mongoose = require('mongoose');
+const GridFsStorage = require('multer-gridfs-storage');
+const gridStream = require('gridfs-stream');
+const User = require('../models/users');
 const loggedIn = require('../config/authenticated');
+
+mongoose.set('useNewUrlParser', true);
+
+const conn = mongoose.createConnection('mongodb://localhost:27017/users');
+
+let gfs;
+
+conn.once('open', () => {
+  gfs = gridStream(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+const storage = new GridFsStorage({
+  url: 'mongodb://localhost:27017/users',
+  file: (req, file) => {
+    const filename = 'file_' + Date.now();
+    const fileInfo = {
+          filename: filename,
+          metadata: {'username': req.user.username},
+          bucketName: 'uploads'
+        };
+    return fileInfo;
+  }
+});
+
+const upload = multer({ storage });
 
 router.get('/', (req, res) => res.sendFile(path.join(__dirname + '../../../public/login.html')));
 
 router.get('/login', (req, res) => res.sendFile(path.join(__dirname + '../../../public/login.html')));
 
 router.get('/profile', loggedIn, (req, res) => {
-  console.log('profile');
   res.sendFile(path.join(__dirname + '../../../public/profile.html'));
+  console.log(req.user);
 });
 
 router.post('/register', (req, res) => {
@@ -64,6 +94,11 @@ router.post('/login', (req, res, next) => {
     //failureFlash: true
   })(req, res, next);
   console.log("Login");
+});
+
+router.post('/upload', loggedIn, upload.single('image'), (req,res) => {
+  console.log(req.user);
+  res.json({ file: req.file });
 });
 
 router.get('/logout', (req, res) => {
